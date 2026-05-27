eb64e41a4fb3e480a83cb7885b5f57c36ac5bf89
Vim-A <ropildavid@gmail.com>
2026-05-28 01:03:52 +0700

    Menghapus User dan Menggunakan Pengguna

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
 use App\Http\Controllers\TicketController;
 use App\Http\Controllers\BookingController;
 
@@ -9,6 +8,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('users', UserController::class);
 Route::resource('tickets', TicketController::class);
 Route::resource('booking', BookingController::class);
