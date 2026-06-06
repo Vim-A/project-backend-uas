7d460ad0184d415ed6e4b74f0e1605b01f5b2401
Vim-A <ropildavid@gmail.com>
2026-06-06 15:57:30 +0700

    memperbarui controller pada schedule

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -2,15 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Schedule;
 use App\Models\Ticket;
-use Illuminate\Http\Request;
 
 class ScheduleController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $schedules = Ticket::with(['concert.artists', 'venue'])
@@ -18,10 +13,7 @@ class ScheduleController extends Controller
             ->orderBy('jam_konser')
             ->get()
             ->groupBy(function ($ticket) {
-                return $ticket->concert_id . '-' .
-                       $ticket->venue_id . '-' .
-                       $ticket->tanggal_konser . '-' .
-                       $ticket->jam_konser;
+                return $ticket->concert_id . '-' . $ticket->venue_id . '-' . $ticket->tanggal_konser . '-' . $ticket->jam_konser;
             })
             ->map(function ($group) {
                 $jadwal = $group->first();
@@ -35,53 +27,5 @@ class ScheduleController extends Controller
             ->values();
 
         return view('schedules.index', compact('schedules'));
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Schedule $schedule)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Schedule $schedule)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Schedule $schedule)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Schedule $schedule)
-    {
-        //
     }
 }
